d415e6e336b20e97e56f467ac3949bbc5b8f1227
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2026-01-02 16:38:33 +0700

    init wallpaper demon

---
+++ src/image_transition.rs
@@ -0,0 +1,102 @@
+use std::{path::Path};
+
+use rayon::{iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator}, slice::ParallelSliceMut};
+
+pub struct ImageTransition {
+    frame_count: u8,
+    velocity: Vec<i8>,
+    initial_image: Vec<u8>,
+    final_image: Vec<u8>,
+}
+
+fn get_velocity(a: &[u8], b: &[u8]) -> Vec<i8> {
+    a.iter().zip(b).map(|(a, b)| {
+        let a = *a as i16;
+        let b = *b as i16;
+        let v = 1;
+        ((b - a).signum() as i8) * v
+    })
+    .collect()
+}
+
+fn apply_velocity(initial_img: &[u8], final_img: &[u8], velocity: &[i8], frame: u8) -> Vec<u8> {
+    initial_img.par_iter()
+        .zip(final_img)
+        .zip(velocity)
+        .map(|((a, b), v)| {
+            let a = *a as i16;
+            let b = *b as i16;
+            let v = *v as i16;
+            let frame = frame as i16;
+            let c = a + v * frame;
+
+            let lower = std::cmp::min(a, b);
+            let higher = std::cmp::max(a, b);
+
+            std::cmp::max(lower, std::cmp::min(c, higher)) as u8
+        })
+        .collect()
+}
+
+impl ImageTransition {
+
+    fn read_image_as_argb<P: AsRef<Path>>(path: P, width: u32, height: u32) -> anyhow::Result<Vec<u8>> {
+
+        let buff = { 
+            let mut current = image::open(path)?;
+            if current.width() != width || current.height() != height {
+                current = current.resize(
+                    width, 
+                    height, 
+                    image::imageops::FilterType::Triangle
+                );
+            }
+            current.to_rgba8()
+        };
+
+        let mut buff = buff.to_vec();
+
+        buff.par_chunks_exact_mut(4).for_each(|buff| {
+            let r = buff[0];
+            let g = buff[1];
+            let b = buff[2];
+            let a = buff[3];
+            buff.copy_from_slice(&[b, g, r, a]);
+        });
+        Ok(buff)
+    }
+
+    pub fn new(images: &[String]) -> Self {
+        let mut it = images.iter();
+        let first_image_path =  it.next().expect("Expect two images");
+        let second_image_path = it.next().expect("Expect two images");
+
+        let width = 1920;
+        let height = 1080;
+
+        let initial_image = Self::read_image_as_argb(first_image_path, width, height).unwrap();
+        let final_image = Self::read_image_as_argb(second_image_path, width, height).unwrap();
+
+        assert_eq!(initial_image.len(), final_image.len());
+
+        let velocity = get_velocity(&initial_image, &final_image);
+
+        Self {
+            initial_image,
+            final_image,
+            frame_count: 0,
+            velocity
+        }
+    }
+
+    pub fn get_frame(&mut self) -> Vec<u8> {
+        let frame = apply_velocity(
+            &self.initial_image, 
+            &self.final_image, 
+            &self.velocity, 
+            self.frame_count
+        );
+        self.frame_count = self.frame_count.saturating_add(1);
+        frame
+    }
+}
---
+++ src/layer.rs
@@ -0,0 +1,357 @@
+use anyhow::Context;
+use smithay_client_toolkit::{compositor::{CompositorHandler, CompositorState}, delegate_compositor, delegate_keyboard, delegate_layer, delegate_output, delegate_pointer, delegate_registry, delegate_seat, delegate_shm, output::{OutputHandler, OutputState}, registry::{ProvidesRegistryState, RegistryState}, registry_handlers, seat::{keyboard::{KeyEvent, KeyboardHandler, Keysym, Modifiers, RawModifiers}, pointer::{PointerEvent, PointerHandler}, Capability, SeatHandler, SeatState}, shell::{wlr_layer::{Anchor, KeyboardInteractivity, Layer, LayerShell, LayerShellHandler, LayerSurface, LayerSurfaceConfigure}, WaylandSurface}, shm::{slot::SlotPool, Shm, ShmHandler}};
+use wayland_client::{globals::registry_queue_init, protocol::{wl_keyboard, wl_output, wl_pointer, wl_seat, wl_shm, wl_surface::{self, WlSurface}}, Connection, EventQueue, QueueHandle};
+
+use crate::image_transition::ImageTransition;
+
+pub struct Monitor {
+    layer: LayerSurface,
+    width: i32,
+    height: i32,
+    configured: bool
+}
+
+pub struct WallpaperLayer {
+    transition: ImageTransition,
+    registry_state: RegistryState,
+    seat_state: SeatState,
+    output_state: OutputState,
+    event_queue: Option<EventQueue<Self>>,
+    layer_shell: LayerShell,
+    compositor_state: CompositorState,
+    pool: SlotPool,
+    shm: Shm,
+    monitors: Vec<Monitor>,
+}
+
+impl WallpaperLayer {
+    pub fn new(args: &[String]) -> anyhow::Result<Self> {
+
+        let transition = ImageTransition::new(args);
+        let conn = Connection::connect_to_env()?;
+        let (globals, event_queue) = registry_queue_init::<WallpaperLayer>(&conn)?;
+        let qh = event_queue.handle();
+
+        let compositor_state = CompositorState::bind(&globals, &qh)?;
+
+        let shm = Shm::bind(&globals, &qh)?;
+
+        let pool = SlotPool::new(1, &shm)?;
+        let layer_shell = LayerShell::bind(&globals, &qh)?;
+
+        Ok(Self {
+            transition,
+            compositor_state,
+            registry_state: RegistryState::new(&globals),
+            seat_state: SeatState::new(&globals, &qh),
+            output_state: OutputState::new(&globals, &qh),
+            event_queue: Some(event_queue),
+            layer_shell,
+            pool,
+            shm,
+            monitors: vec![]
+        })
+    }
+
+    pub fn run(&mut self) -> anyhow::Result<()> {
+        let Some(mut evt_queue) = self.event_queue.take() else {
+            return Ok(())
+        };
+
+        loop {
+            evt_queue.blocking_dispatch(self)?;
+        }
+    }
+
+    pub fn handle_new_output(
+        &mut self,
+        qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) -> anyhow::Result<()> {
+        let output_info = self.output_state.info(&output).context("Failed to get output info")?;
+        let monitor_name = output_info.name.context("Failed to get monitor_name")?;
+        println!("New Monitor: {}", monitor_name);
+        let (width, height) = output_info.logical_size.context("Failed to get monitor width and height")?;
+        let surface = self.compositor_state.create_surface(qh);
+        let layer = self.layer_shell.create_layer_surface(
+            qh,
+            surface,
+            Layer::Background, 
+            Some("background_layer"), 
+            Some(&output)
+        );
+        layer.set_anchor(Anchor::BOTTOM);
+        layer.set_keyboard_interactivity(KeyboardInteractivity::None);
+        layer.set_size(width as u32, height as u32);
+        layer.commit();
+        let monitor = Monitor { width, height, layer, configured: false };
+        self.monitors.push(monitor);
+        Ok(())
+    }
+
+    fn render(&mut self, qh: &QueueHandle<Self>, surface: &WlSurface) -> anyhow::Result<()> {
+        let Some(monitor) = self.monitors.iter().find(|m| m.layer.wl_surface() == surface) else {
+            return Ok(());
+        };
+        if !monitor.configured {
+            return Ok(())
+        }
+        let frame = self.transition.get_frame();
+
+        let width = monitor.width;
+        let height = monitor.height;
+        let stride = width * 4;
+        let (buffer, canvas) = self.pool.create_buffer(
+            monitor.width,
+            monitor.height,
+            stride,
+            wl_shm::Format::Argb8888
+        )?;
+
+        canvas.copy_from_slice(&frame);
+        monitor.layer.wl_surface().damage_buffer(0, 0, width, height);
+        monitor.layer.wl_surface().frame(qh, monitor.layer.wl_surface().clone());
+
+        buffer.attach_to(monitor.layer.wl_surface())?;
+        monitor.layer.commit();
+
+        Ok(())
+        
+    }
+}
+
+impl CompositorHandler for WallpaperLayer {
+    fn scale_factor_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _new_factor: i32,
+    ) {
+        // Not needed for this example.
+    }
+
+    fn transform_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _new_transform: wl_output::Transform,
+    ) {
+        // Not needed for this example.
+    }
+
+    fn frame(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        surface: &wl_surface::WlSurface,
+        _time: u32,
+    ) {
+        self.render(qh, surface).unwrap();
+    }
+
+    fn surface_enter(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _output: &wl_output::WlOutput,
+    ) {
+        // Not needed for this example.
+    }
+
+    fn surface_leave(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _output: &wl_output::WlOutput,
+    ) {
+        // Not needed for this example.
+    }
+}
+
+impl OutputHandler for WallpaperLayer {
+    fn output_state(&mut self) -> &mut OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+        self.handle_new_output(qh, output).unwrap();
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+}
+
+impl LayerShellHandler for WallpaperLayer {
+    fn closed(&mut self, _conn: &Connection, _qh: &QueueHandle<Self>, _layer: &LayerSurface) { }
+
+    fn configure(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        layer: &LayerSurface,
+        _configure: LayerSurfaceConfigure,
+        _serial: u32,
+    ) {
+        if let Some(monitor) = self.monitors.iter_mut().find(|m| m.layer == *layer) {
+            monitor.configured = true;
+            self.render(qh, layer.wl_surface()).unwrap();
+        }
+    }
+}
+
+impl SeatHandler for WallpaperLayer {
+    fn seat_state(&mut self) -> &mut SeatState {
+        &mut self.seat_state
+    }
+
+    fn new_seat(&mut self, _: &Connection, _: &QueueHandle<Self>, _: wl_seat::WlSeat) {}
+
+    fn new_capability(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: wl_seat::WlSeat,
+        _: Capability,
+    ) {
+    }
+
+    fn remove_capability(
+        &mut self,
+        _conn: &Connection,
+        _: &QueueHandle<Self>,
+        _: wl_seat::WlSeat,
+        _: Capability,
+    ) {
+    }
+
+    fn remove_seat(&mut self, _: &Connection, _: &QueueHandle<Self>, _: wl_seat::WlSeat) {}
+}
+
+impl KeyboardHandler for WallpaperLayer {
+    fn enter(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_keyboard::WlKeyboard,
+        _: &wl_surface::WlSurface,
+        _: u32,
+        _: &[u32],
+        _: &[Keysym],
+    ) {
+    }
+
+    fn leave(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_keyboard::WlKeyboard,
+        _: &wl_surface::WlSurface,
+        _: u32,
+    ) {
+    }
+
+    fn press_key(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _: &wl_keyboard::WlKeyboard,
+        _: u32,
+        _event: KeyEvent,
+    ) {
+    }
+
+    fn repeat_key(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _keyboard: &wl_keyboard::WlKeyboard,
+        _serial: u32,
+        event: KeyEvent,
+    ) {
+        println!("Key repeat: {event:?}");
+    }
+
+    fn release_key(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_keyboard::WlKeyboard,
+        _: u32,
+        event: KeyEvent,
+    ) {
+        println!("Key release: {event:?}");
+    }
+
+    fn update_modifiers(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_keyboard::WlKeyboard,
+        _serial: u32,
+        modifiers: Modifiers,
+        _raw_modifiers: RawModifiers,
+        _layout: u32,
+    ) {
+        println!("Update modifiers: {modifiers:?}");
+    }
+}
+
+impl PointerHandler for WallpaperLayer {
+    fn pointer_frame(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _pointer: &wl_pointer::WlPointer,
+        _events: &[PointerEvent],
+    ) {
+    }
+}
+
+impl ShmHandler for WallpaperLayer {
+    fn shm_state(&mut self) -> &mut Shm {
+        &mut self.shm
+    }
+}
+
+impl ProvidesRegistryState for WallpaperLayer {
+    fn registry(&mut self) -> &mut RegistryState {
+        &mut self.registry_state
+    }
+    registry_handlers![OutputState, SeatState];
+}
+
+
+delegate_compositor!(WallpaperLayer);
+delegate_output!(WallpaperLayer);
+delegate_seat!(WallpaperLayer);
+delegate_keyboard!(WallpaperLayer);
+delegate_pointer!(WallpaperLayer);
+delegate_shm!(WallpaperLayer);
+
+delegate_layer!(WallpaperLayer);
+
+delegate_registry!(WallpaperLayer);
+
+
